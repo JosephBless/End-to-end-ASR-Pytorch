b9170f4defc27dcfe59d3082f4464c3b985ef546
Leo Yang <leo19941227@gmail.com>
2021-01-31 16:30:54 +0800

    refactor & switch apex.amp to torch.cuda.amp

--- bin/train_asr.py
+++ bin/train_asr.py
@@ -2,6 +2,7 @@ import torch
 from torch.nn.utils.rnn import pad_sequence
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import is_initialized, get_rank, get_world_size
+from torch.cuda.amp import autocast, GradScaler
 
 from src.asr import ASR
 from src.optim import Optimizer
@@ -41,8 +42,6 @@ class Solver(BaseSolver):
                          self.curriculum > 0, **self.config['data'],
                          wav_only=self.paras.upstream is not None,
                          dryrun=self.paras.dryrun)
-        if self.paras.upstream is not None:
-            self.set_upstream()
         self.verbose(msg)
 
     def set_upstream(self):
@@ -94,6 +93,14 @@ class Solver(BaseSolver):
         return feat, feat_len
 
     def set_model(self):
+        model_paras = []
+
+        ''' Setup pretrained upstream'''
+        if self.paras.upstream is not None:
+            self.set_upstream()
+            if self.paras.upstream_trainable:
+                model_paras.append({'params': self.upstream.parameters()})
+
         ''' Setup ASR model and optimizer '''
         # Model
         init_adadelta = self.config['hparas']['optimizer'] == 'Adadelta'
@@ -106,7 +113,7 @@ class Solver(BaseSolver):
             setattr(self.model, 'ctc_weight', self.model.module.ctc_weight)
 
         self.verbose(self.model.create_msg())
-        model_paras = [{'params': self.model.parameters()}]
+        model_paras.append({'params': self.model.parameters()})
 
         # Losses
         self.seq_loss = torch.nn.CrossEntropyLoss(ignore_index=0)
@@ -131,8 +138,13 @@ class Solver(BaseSolver):
         self.optimizer = Optimizer(model_paras, **self.config['hparas'])
         self.verbose(self.optimizer.create_msg())
 
-        # Enable AMP if needed
-        self.enable_apex()
+        # Loss scaler
+        self.scaler = GradScaler(enabled=self.paras.amp)
+
+        # param list for grad clipping
+        self.params_list = []
+        for model_para in model_paras:
+            self.params_list += list(model_para['params'])
 
         # Automatically load pre-trained model if self.paras.load is given
         self.load_ckpt()
@@ -144,66 +156,74 @@ class Solver(BaseSolver):
         self.verbose('Total training steps {}.'.format(
             human_format(self.max_step)))
         ctc_loss, att_loss, emb_loss = None, None, None
-        n_epochs = 0
         self.timer.set()
 
         while self.step < self.max_step:
             # Renew dataloader to enable random sampling
-            if self.curriculum > 0 and n_epochs == self.curriculum:
+            if self.curriculum > 0 and self.n_epochs == self.curriculum:
                 self.verbose(
-                    'Curriculum learning ends after {} epochs, starting random sampling.'.format(n_epochs))
+                    'Curriculum learning ends after {} epochs, starting random sampling.'.format(self.n_epochs))
                 self.tr_set, _, _, _, _, _ = \
                     load_dataset(self.paras.njobs, self.paras.gpu, self.paras.pin_memory,
                                  False, **self.config['data'])
 
             if is_initialized():
-                self.tr_set.sampler.set_epoch(n_epochs)
+                self.tr_set.sampler.set_epoch(self.n_epochs)
 
             for data in self.tr_set:
                 # Pre-step : update tf_rate/lr_rate and do zero_grad
                 tf_rate = self.optimizer.pre_step(self.step)
                 total_loss = 0
 
-                # Fetch data
-                feat, feat_len, txt, txt_len = self.fetch_data(data)
-                self.timer.cnt('rd')
+                with autocast(enabled=self.paras.amp):
+                    # Fetch data
+                    feat, feat_len, txt, txt_len = self.fetch_data(data)
+                    self.timer.cnt('rd')
 
-                # Forward model
-                # Note: txt should NOT start w/ <sos>
-                ctc_output, encode_len, att_output, att_align, dec_state = \
-                    self.model(feat, feat_len, max(txt_len).item(), tf_rate=tf_rate,
-                               teacher=txt, get_dec_state=self.emb_reg)
+                    # Forward model
+                    # Note: txt should NOT start w/ <sos>
+                    ctc_output, encode_len, att_output, att_align, dec_state = \
+                        self.model(feat, feat_len, max(txt_len).item(), tf_rate=tf_rate,
+                                teacher=txt, get_dec_state=self.emb_reg)
 
-                # Plugins
-                if self.emb_reg:
-                    emb_loss, fuse_output = self.emb_decoder(
-                        dec_state, att_output, label=txt)
-                    total_loss += self.emb_decoder.weight*emb_loss
+                    # Plugins
+                    if self.emb_reg:
+                        emb_loss, fuse_output = self.emb_decoder(
+                            dec_state, att_output, label=txt)
+                        total_loss += self.emb_decoder.weight*emb_loss
 
-                # Compute all objectives
-                if ctc_output is not None:
-                    if self.paras.cudnn_ctc:
-                        ctc_loss = self.ctc_loss(ctc_output.transpose(0, 1),
-                                                 txt.to_sparse().values().to(device='cpu', dtype=torch.int32),
-                                                 [ctc_output.shape[1]] *
-                                                 len(ctc_output),
-                                                 txt_len.cpu().tolist())
-                    else:
-                        ctc_loss = self.ctc_loss(ctc_output.transpose(
-                            0, 1), txt, encode_len, txt_len)
-                    total_loss += ctc_loss*self.model.ctc_weight
+                    # Compute all objectives
+                    if ctc_output is not None:
+                        if self.paras.cudnn_ctc:
+                            ctc_loss = self.ctc_loss(ctc_output.transpose(0, 1),
+                                                    txt.to_sparse().values().to(device='cpu', dtype=torch.int32),
+                                                    [ctc_output.shape[1]] *
+                                                    len(ctc_output),
+                                                    txt_len.cpu().tolist())
+                        else:
+                            ctc_loss = self.ctc_loss(ctc_output.transpose(
+                                0, 1), txt, encode_len, txt_len)
+                        total_loss += ctc_loss*self.model.ctc_weight
 
-                if att_output is not None:
-                    b, t, _ = att_output.shape
-                    att_output = fuse_output if self.emb_fuse else att_output
-                    att_loss = self.seq_loss(
-                        att_output.view(b*t, -1), txt.view(-1))
-                    total_loss += att_loss*(1-self.model.ctc_weight)
+                    if att_output is not None:
+                        b, t, _ = att_output.shape
+                        att_output = fuse_output if self.emb_fuse else att_output
+                        att_loss = self.seq_loss(
+                            att_output.view(b*t, -1), txt.view(-1))
+                        total_loss += att_loss*(1-self.model.ctc_weight)
 
                 self.timer.cnt('fw')
 
                 # Backprop
-                grad_norm = self.backward(total_loss)
+                self.timer.set()
+                self.scaler.scale(total_loss).backward()
+                
+                self.scaler.unscale_(self.optimizer)
+                grad_norm = torch.nn.utils.clip_grad_norm_(self.params_list, self.GRAD_CLIP)
+                self.scaler.step(self.optimizer)
+                self.scaler.update()
+
+                self.timer.cnt('bw')
                 self.step += 1
 
                 # Logger
@@ -232,7 +252,7 @@ class Solver(BaseSolver):
                 self.timer.set()
                 if self.step > self.max_step:
                     break
-            n_epochs += 1
+            self.n_epochs += 1
 
         if hasattr(self, 'log'):
             self.log.close()
